8bfec58e1b156f97bac30592ed676bd3314b5f21
Edward Pina <en.pina1@gmail.com>
2021-11-11 21:48:09 +0100

    hopefully

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_191031) do
+ActiveRecord::Schema.define(version: 2021_02_19_155458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
+    t.text "comment"
     t.bigint "movie_id", null: false
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -35,15 +35,15 @@ ActiveRecord::Schema.define(version: 2021_11_10_191031) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "rating"
     t.bigint "list_id", null: false
+    t.text "comment"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_reviews_on_list_id"
